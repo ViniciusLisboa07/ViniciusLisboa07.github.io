46a0ebd22d60acd88e52e8a8ff89761f873f4bfa
ViniciusLisboa07 <viniciuslisboa1001@gmail.com>
2025-01-28 23:47:10 -0300

    feat: remove basePath and assetPrefix from Next.js configuration

--- next.config.ts
+++ next.config.ts
@@ -2,8 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: 'export',
-  basePath: '/ViniciusLisboa07.github.io',
-  assetPrefix: '/ViniciusLisboa07.github.io/',
   /* config options here */
 };
 
