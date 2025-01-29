3c90e3aee7dc2daa6d06f2aaad11d03f653b966c
ViniciusLisboa07 <viniciuslisboa1001@gmail.com>
2025-01-28 22:59:05 -0300

    feat: comment out basePath in Next.js configuration for portfolio website

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: 'export',
-  basePath: '/meu-portfolio',
+  // basePath: '/meu-portfolio',
   /* config options here */
 };
 
