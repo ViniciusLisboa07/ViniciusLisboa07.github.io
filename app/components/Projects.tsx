35b30ccf6861cb138c0252e5c14829364b252edf
ViniciusLisboa07 <viniciuslisboa1001@gmail.com>
2025-08-07 23:38:44 -0300

    feat: update styles and localization for improved blog presentation and user experience

--- app/components/Projects.tsx
+++ app/components/Projects.tsx
@@ -2,7 +2,6 @@
 
 import { motion } from "framer-motion"
 import { useTranslation } from 'react-i18next'
-import Image from "next/image"
 
 export default function Projects() {
   const { t } = useTranslation()
